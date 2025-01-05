b8a7cc6cb6ad81aef5b69d8fce8f433fbb912917
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-05 21:42:32 +0600

    use variable on string

--- 6. What is Variables/main.js
+++ 6. What is Variables/main.js
@@ -1,5 +1,8 @@
 var name = "Shihab";
 var age = 25;
 
-console.log(name);
-console.log(age);
+// console.log(name);
+// console.log(age);
+
+console.log("My name is ", name);
+console.log("I am " + age + " old");
