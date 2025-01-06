e8e1650358056f8e341c655399965f851def6137
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-06 08:57:48 +0600

    learn Number data type

---
+++ 9. JS Basic Numbers/main.js
@@ -0,0 +1,20 @@
+/* Number Type */
+
+var num = 123;  // number lateral
+var pi = 3.14
+console.log(num)
+console.log(pi)
+
+var num1 = Number(45)
+// var num2 = Number('45')
+var num2 = Number('45.5')
+
+console.log(Number.parseFloat(num))
+console.log(Number.parseInt(pi))
+console.log(Number.parseInt(num2))
+
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_SAFE_INTEGER)
+
+console.log(1/0)
+console.log("abc" * 10)
