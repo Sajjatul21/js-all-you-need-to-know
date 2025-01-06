a7ef3fb4718ca35230f2106cfd9a435e0189f742
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-06 13:05:18 +0600

    num to string

---
+++ 13. JS Type Conversion/main.js
@@ -0,0 +1,11 @@
+var str = '10000'
+var n = 10;
+console.log(str * n) // In JavaScript, when you use the multiplication operator (*) between a string and a number, the string is automatically converted to a number before the multiplication operation.
+
+console.log(str + n) // concat
+
+// type conversion string to number 
+console.log(Number(str)+n)
+console.log(Number.parseInt(str)+ n)
+
+console.log(n.toString())
