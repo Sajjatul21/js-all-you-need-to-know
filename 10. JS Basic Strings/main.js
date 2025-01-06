15bfa9879e8f989f84b68a356b16e6d47cca7a5f
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-06 09:05:09 +0600

    learn string lateral and string constructor

---
+++ 10. JS Basic Strings/main.js
@@ -0,0 +1,15 @@
+/*----------- String ----------*/
+
+//  string lateral 
+
+var str = 'string'
+var str2 = "string2"
+var str3 = `string3`
+console.log(str, str2, str3)
+
+// string constructor
+
+var str4 = String('fafd')
+var str5 = String(123)
+var str6 = String(3.1416)
+console.log(str4,str5, str6)
