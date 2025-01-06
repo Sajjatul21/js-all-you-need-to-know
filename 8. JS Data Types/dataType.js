5985327945004eda9f470823d9bd6f1e4eed4830
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-06 08:41:14 +0600

    learn dataType

---
+++ 8. JS Data Types/dataType.js
@@ -0,0 +1,13 @@
+// primitive =>  Primitive type are defined by Javascript
+
+    1. Number (1,2,3,6.7)
+    2. String ("This is text")
+    3. Boolen (True or False)
+    4. undefined
+    5. null
+
+// object => object type are user defined data type 
+
+    1. Array
+    2. Object 
+    3. Function 
