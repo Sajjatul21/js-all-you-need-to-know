8bfa2e113dcdcc55534e10341c70871afc8cc74f
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-07 14:09:19 +0600

    floor, ceil, round, max, min

--- 17. JS Math Functions/main.js
+++ 17. JS Math Functions/main.js
@@ -3,3 +3,11 @@ console.log(Math.PI)
 
 var n = -4.56
 console.log(Math.abs(n))
+
+n = 4.65;
+console.log(Math.floor(n))
+console.log(Math.ceil(n))
+console.log(Math.round(n))
+
+console.log(Math.max(400,600, 200))
+console.log(Math.min(400,600, 200))
