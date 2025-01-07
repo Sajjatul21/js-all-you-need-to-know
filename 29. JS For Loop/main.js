e9da49d13e9aa99b3b15340e803a6b92af37297f
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-07 20:04:23 +0600

    print sum beutyfully

--- 29. JS For Loop/main.js
+++ 29. JS For Loop/main.js
@@ -10,15 +10,17 @@ for (var i = 0; i < 100; i++) {
     console.log((i + 1) + " Sajjatul Islam");
 }
 
-for(var i = 1; i<=10;i++){
-    if(i%2 == 0){
-        console.log(i)
+for (var i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
     }
 }
 
 
 var sum = 0;
-for ( var i = 1; i<=10;i++){
-    sum = sum + i;
+for (var i = 1; i <= 10; i++) {
+
+    console.log(sum + " + " + i + " = " + (sum + i));
+    sum += i; // sum = sum + i;
 }
-console.log(sum)
+console.log("total sum",sum);
