d0b1d4e1146b6aadfa90249d32e4ad79f0f7959e
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-07 16:30:39 +0600

    learn else if

---
+++ 23. JS Else If Condition/main.js
@@ -0,0 +1,24 @@
+var a = 20;
+var b = 20;
+
+if (a > b) {
+    console.log(" a is greater than b");
+}
+else if (a < b) {
+    console.log("b is greater than a");
+}
+else {
+    console.log("they both are same");
+}
+
+
+var n = 5;
+if (n % 2 == 0) {
+    console.log(n + " is Even Number");
+}
+else if (n % 2 == 1) {
+    console.log(n + " is Odd Number");
+}
+else {
+    console.log(n + " is zero");
+}
