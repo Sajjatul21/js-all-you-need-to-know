96daec08a045cb29fb131da2bf3a2014863079ea
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-07 19:59:13 +0600

    print even number

--- 29. JS For Loop/main.js
+++ 29. JS For Loop/main.js
@@ -9,3 +9,9 @@ for(initializer, condition, increment or decrement){
 for (var i = 0; i < 100; i++) {
     console.log((i + 1) + " Sajjatul Islam");
 }
+
+for(var i = 1; i<=10;i++){
+    if(i%2 == 0){
+        console.log(i)
+    }
+}
