fa4e991d3140ee89d48eabd8650d068767197c1d
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-07 14:30:55 +0600

    year, month, date, hour, minute, second

--- 18. JS Date Functions/main.js
+++ 18. JS Date Functions/main.js
@@ -4,3 +4,11 @@ console.log(date)
 console.log(date.toDateString())
 console.log(date.toTimeString())
 console.log(date.toLocaleString())
+
+
+console.log("full year",date.getFullYear())
+console.log("month",date.getMonth())
+console.log("date",date.getDate())
+console.log("hour",date.getHours())
+console.log("minute",date.getMinutes())
+console.log("second",date.getSeconds())
