42542b1c54b54ffbb9fafa7f3c490b1d0736a25d
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-07 09:03:08 +0600

    assignment operator

--- 15. JS Operators/main.js
+++ 15. JS Operators/main.js
@@ -1,19 +1,41 @@
-var a = 11;
-var b = 3;
-console.log(a % b);
+// var a = 11;
+// var b = 3;
+// console.log(a % b);
 
-/* Incremental */
+// /* Incremental */
 
-//  Pre incremental
-console.log(++a)
+// //  Pre incremental
+// console.log(++a)
 
-// Post incremental
-console.log(a++)   //post increment: First the output will show then increase
+// // Post incremental
+// console.log(a++)   //post increment: First the output will show then increase
+// console.log(a)
+
+// // Pre Decrement 
+// console.log(--b)
+
+// // Post Decrement
+// console.log(b--)
+// console.log(b)
+
+
+
+// Assignment Operator
+
+var a = 10
+var b =20;
+
+a += b  // a = a+b
 console.log(a)
 
-// Pre Decrement 
-console.log(--b)
+a -= b  
+console.log(a)
 
-// Post Decrement
-console.log(b--)
-console.log(b)
+a *= b  
+console.log(a)
+
+a /= b  
+console.log(a)
+
+a %= b  
+console.log(a)
