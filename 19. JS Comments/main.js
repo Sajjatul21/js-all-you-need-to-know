e50ab9320d046ba0fcccba4ac41923ba96dcd615
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-07 14:46:31 +0600

    comment

---
+++ 19. JS Comments/main.js
@@ -0,0 +1,18 @@
+// single line comment
+
+
+// var date = new Date();
+// console.log(date);
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
+// console.log(date.toLocaleString());
+
+
+/* multi line comment */
+
+/* console.log("full year", date.getFullYear());
+console.log("month", date.getMonth());
+console.log("date", date.getDate());
+console.log("hour", date.getHours());
+console.log("minute", date.getMinutes());
+console.log("second", date.getSeconds()); */
