2d85a13d54576de7584c32778fa5a7f3745daf47
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-07 20:20:30 +0600

    another use case while loop

--- 30. JS While Loop/main.js
+++ 30. JS While Loop/main.js
@@ -1,5 +1,19 @@
 var i = 0;
 while (i < 10) {
-    console.log(i,'Shihab');
+    console.log(i, 'Shihab');
     i++;
+}
+
+// when we know starting and ending point we use for loop 
+
+var isRunning = true;
+while (isRunning) {
+    var rand = Math.floor(Math.random() * 10 + 1);
+    if (rand == 9) {
+        console.log("winner winner chicken dinner");
+        isRunning = false;
+    }
+    else {
+        console.log('You have got ' + rand);
+    }
 }
