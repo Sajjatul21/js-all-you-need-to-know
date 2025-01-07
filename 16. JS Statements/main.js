8fcae750600bc4964d33ccca353fc26e4c845166
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-07 13:30:02 +0600

    statement

---
+++ 16. JS Statements/main.js
@@ -0,0 +1,7 @@
+// Statement: যে কোন প্রোগ্রামিং ল্যাংগুয়েজে যখন  কোন কোড বা  লাইন অফ কোড কোনে মিনিংফুল অর্থ বহন করে তখন তাকে Statement বলে।
+
+console.log("Hello World");
+var str = "String";
+
+var number = 10 + (30 / 5) * 5;
+var a = 10;
