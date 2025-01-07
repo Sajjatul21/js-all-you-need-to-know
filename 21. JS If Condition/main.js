132245c56d42565ecb78355b1e6235637ab184f2
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-07 16:17:35 +0600

    learn if condition

---
+++ 21. JS If Condition/main.js
@@ -0,0 +1,19 @@
+var a = 20;
+var b = 10;
+
+if (a > b) {
+    console.log('a is greater then b');
+}
+
+if (a < b) {
+    console.log("b is greater than a");
+}
+
+
+var n = 5;
+if (n % 2 == 0) {
+    console.log(n + " is Even number");
+}
+if (n % 2 == 1) {
+    console.log(n + " is Odd number");
+}
