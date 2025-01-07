0c8331c8eaef448897aaf477d6a9095b31801431
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-07 14:15:30 +0600

    power and square root

--- 17. JS Math Functions/main.js
+++ 17. JS Math Functions/main.js
@@ -11,3 +11,11 @@ console.log(Math.round(n))
 
 console.log(Math.max(400,600, 200))
 console.log(Math.min(400,600, 200))
+
+
+console.log(Math.pow(2,3)) // two to the power three.
+console.log(Math.pow(3,5))
+
+
+console.log(Math.sqrt(9)) // nine square root
+console.log(Math.sqrt(64))
