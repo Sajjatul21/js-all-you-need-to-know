4944d5261eb278fd940bb09a5363c0540381b19e
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-07 09:09:48 +0600

    comparison operator

--- 15. JS Operators/main.js
+++ 15. JS Operators/main.js
@@ -22,20 +22,33 @@
 
 // Assignment Operator
 
-var a = 10
-var b =20;
+var a = 10;
+var b = 20;
 
-a += b  // a = a+b
-console.log(a)
+a += b;  // a = a+b
+console.log(a);
 
-a -= b  
-console.log(a)
+a -= b;
+console.log(a);
 
-a *= b  
-console.log(a)
+a *= b;
+console.log(a);
 
-a /= b  
-console.log(a)
+a /= b;
+console.log(a);
 
-a %= b  
-console.log(a)
+a %= b;
+console.log(a);
+
+var num1 = 40;
+var num2 = 30;
+
+console.log(num1 == num2);
+console.log(num1 != num2);
+console.log(num1 > num2);
+console.log(num1 < num2);
+
+num1 = 30;
+
+console.log(num1 >= num2)
+console.log(num1 <= num2)
