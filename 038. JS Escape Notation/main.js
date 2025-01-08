0fe945c3e299daa45dc08b6f627f5fd82cbc9e20
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-08 21:14:23 +0600

    escape notatin

---
+++ 038. JS Escape Notation/main.js
@@ -0,0 +1,8 @@
+var str = 'This is a \'String\'';
+console.log(str)
+var str1 = 'This is a \nString';
+console.log(str1)
+var str2 = 'This is a \tString';
+console.log(str2)
+var str3 = 'This is a \\String';
+console.log(str3)
