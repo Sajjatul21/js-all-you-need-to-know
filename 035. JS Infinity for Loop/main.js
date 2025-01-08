bb25a39d791299dc9b7fa6fb0592ed8da99cc34f
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-08 20:38:54 +0600

    we make e infinity for loop use this code

---
+++ 035. JS Infinity for Loop/main.js
@@ -0,0 +1,11 @@
+var isRunning = true
+while(isRunning){
+    var rand = Math.floor(Math.random()*10+1)
+    if(rand == 9){
+        console.log("Winner winner chicken dinner")
+        break;
+    }
+    else{
+        console.log("you have got",rand)
+    }
+}
