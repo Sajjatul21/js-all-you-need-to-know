eb033332b10409e90d9d38ba4945ede4782b121b
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-08 20:12:10 +0600

    complete nested chalange

--- 032. JS Nested Loops/main.js
+++ 032. JS Nested Loops/main.js
@@ -9,7 +9,9 @@
 
 var n = 5;
 for (var i = 1; i <= n; i++) {
+    var result = '';
     for (var j = 1; j <= i; j++) {
-        console.log(j);
+        result = result + j + ' ';
     }
+    console.log(result);
 }
