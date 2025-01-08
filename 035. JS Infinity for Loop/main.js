17a01e4f43ace2c2eb4d04df6bcc6256f28517b5
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-08 20:51:52 +0600

    make infinity useing for loop

--- 035. JS Infinity for Loop/main.js
+++ 035. JS Infinity for Loop/main.js
@@ -1,11 +1,22 @@
-var isRunning = true
-while(isRunning){
-    var rand = Math.floor(Math.random()*10+1)
-    if(rand == 9){
-        console.log("Winner winner chicken dinner")
+/* var isRunning = true;
+while (isRunning) {
+    var rand = Math.floor(Math.random() * 10 + 1);
+    if (rand == 9) {
+        console.log("Winner winner chicken dinner");
         break;
     }
-    else{
-        console.log("you have got",rand)
+    else {
+        console.log("you have got", rand);
+    }
+} */
+
+for (; ;) {
+    var rand = Math.floor(Math.random() * 10 + 1);
+    if (rand == 9) {
+        console.log("Winner winner chicken dinner");
+        break;
+    }
+    else {
+        console.log("you have got", rand);
     }
 }
