cb3f316f807485be7df0a321459c33d446acc8ea
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-09 03:59:34 +0600

    take out length to usig charAt() and also know easy way find out length

--- 041. JS String Length/main.js
+++ 041. JS String Length/main.js
@@ -7,3 +7,20 @@ console.log(str2);
 var str3 = str.charAt(100);
 console.log(str3);
 console.log(typeof str3);
+
+
+str = 'Some String';
+var length = 0;
+
+while (true) {
+    if (str.charAt(length) == '') {
+        break;
+    }
+    else {
+        length++;
+    }
+}
+console.log(length);
+
+//easy way to find out string length
+console.log('Sajjatul Islam'.length);
