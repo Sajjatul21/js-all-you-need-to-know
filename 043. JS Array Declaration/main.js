08631cfa8d39b1e03bbc3b08b5f0c5fc41333aff
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-09 04:21:42 +0600

    Array letaral and constructor

---
+++ 043. JS Array Declaration/main.js
@@ -0,0 +1,19 @@
+// array lateral
+var arr = [1, 2, 3, 4, 5];
+console.log(arr);
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[4]);
+
+arr[2] = 25;
+arr[10] = 10;
+console.log(arr);
+console.log(arr.length);
+
+// array constructor
+var arr2 = Array();
+console.log(arr2);
+
+var arr3 = Array(10, 20, 30);
+console.log(arr3);
