039610c4d9b2f75bdaf233d42e552f795ae21bb9
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-08 21:06:25 +0600

    comvert num to string many way

--- 037. String Literal vs Constructor/main.js
+++ 037. String Literal vs Constructor/main.js
@@ -5,3 +5,12 @@ console.log(str1);
 // string constructor
 var str2 = String("Sajjatul Islam");
 console.log(str2);
+
+
+var n = 10;
+var st3 = n + ''
+var st4 = n.toString()
+var st5 = String(n)
+console.log(st3)
+console.log(st4)
+console.log(st5)
