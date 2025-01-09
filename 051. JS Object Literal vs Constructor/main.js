d3ad4c5570a583c96b5f0db22d01158879fb4549
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-09 17:35:02 +0600

    object

---
+++ 051. JS Object Literal vs Constructor/main.js
@@ -0,0 +1,24 @@
+/* var obj = {}
+console.log(typeof obj)
+obj.x = 10
+console.log(obj)
+ */
+
+// object ltaral
+var point = {
+    x: 10,
+    y: 20
+};
+console.log(point);
+point.x = 5;
+point.z = 30;
+console.log(point);
+
+// Object constructor 
+var obj = Object()
+obj.a = 10
+console.log(obj)
+
+var obj1 = new Object()
+obj1.b = 20;
+console.log(obj1)
