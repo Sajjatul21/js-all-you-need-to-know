f4296c357f8734789c7f8923ce55becccbfdd2cf
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-09 19:26:06 +0600

    get property in object using dot notation and array notation

---
+++ 52. Accessing Object Properties in Javascript/main.js
@@ -0,0 +1,40 @@
+var obj = {
+    x: 10,
+    y: 20
+};
+
+// access object property using dot notation
+console.log(obj.x);
+console.log(obj.y);
+
+
+// access object property using bracket notation
+
+console.log(obj["x"]); // when we access a property  using  array notation it must be wrap quotation. 
+
+// "Array notation is necessary for dynamic property names and property names containing special characters."
+
+/* 
+    প্রোপার্টির নাম একটি ভেরিয়েবলে থাকে: কখনও কখনও আপনি কোড লেখার সময় সঠিক প্রোপার্টির নাম জানেন না,
+    তাই আপনি এটি একটি ভেরিয়েবলে সংরক্ষণ করেন এবং তারপর সেই ভেরিয়েবলের মাধ্যমে মানটি পান। তখন আপনি array notation ব্যবহার করবেন। আবার
+    যদি আমরা না যানি, আমাদের কোন প্রোপাটি টা access করতে হবে তখন আমরা array notation ব্যবহার করব।
+*/
+
+var person = {
+    name: "johan",
+    age: 32
+};
+// console.log(person['name'])
+var props = "name";
+console.log(person[props]);
+
+/*প্রোপার্টির নামের মধ্যে স্পেস বা বিশেষ ক্যারেক্টার থাকে: যদি প্রোপার্টির নাম একটি সাধারণ শব্দ না হয় 
+(যেমন first name), ডট নোটেশন কাজ করবে না, এবং আপনাকে ব্র্যাকেট নোটেশন ব্যবহার করতে হবে।
+*/
+
+var person1 = {
+    "fast Name": "jack johan ",
+    age: 32
+};
+
+console.log(person1["fast Name"]);
