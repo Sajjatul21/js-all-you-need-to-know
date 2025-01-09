2c483cb1a241cfe1e12f44d4d962a9e879d45446
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-09 15:59:39 +0600

    if data is not found the data not found show one time

--- 046. JS Search Value in Array/main.js
+++ 046. JS Search Value in Array/main.js
@@ -1,13 +1,16 @@
 var arr = [1, 2, 52, 6, 2, 74, 83, 26];
 
 var find = 10;
-
+// var find = 2;
+var isFound = false;
 for (var i = 0; i < arr.length; i++) {
     if (arr[i] == find) {
         console.log('Data found at Index', i);
+        isFound = true;
         break;
     }
-    else {
-        console.log('Data not found');
-    }
+}
+
+if (!isFound) {
+    console.log("Data not found");
 }
