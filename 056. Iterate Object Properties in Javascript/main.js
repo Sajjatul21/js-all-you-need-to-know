8c9552024cf7318486fea270704a2a6ecd2f1f5a
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-09 20:34:40 +0600

    for in loop

--- 056. Iterate Object Properties in Javascript/main.js
+++ 056. Iterate Object Properties in Javascript/main.js
@@ -6,3 +6,15 @@ var obj = {
 };
 console.log("x" in obj); // property name should be string
 console.log("p" in obj);
+
+var obj1 = {
+    x: 10,
+    y: 20,
+    z: 30
+};
+
+for (var i in obj) {
+    // console.log(i); //for in loop work on property
+    // console.log(obj[i]) // we use array notation to access obj1 property because property under variable and property are dynamic
+    console.log(i + ": " + obj[i]);
+}
