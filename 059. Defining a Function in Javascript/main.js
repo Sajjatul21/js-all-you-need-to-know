d2eefe385b3836e17caa19b373ea77648e15384f
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-09 20:58:55 +0600

    function define

---
+++ 059. Defining a Function in Javascript/main.js
@@ -0,0 +1,15 @@
+function functionName() {
+
+}
+
+function add() {
+    var a = 10;
+    var b = 20;
+    console.log(a + b);
+}
+
+function sub() {
+    var a = 10;
+    var b = 20;
+    console.log(a - b);
+}
