80f153de4b8092f2f9bab6ecfb015c7f74ba5416
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-09 12:02:25 +0600

    print even number

--- 044. JS Array Traversing/main.js
+++ 044. JS Array Traversing/main.js
@@ -8,7 +8,14 @@ for (var i = 0; i < arr.length; i++) {
 
 var arr1 = [5, 4, 3, 10, 8, 2, 7];
 var sum = 0;
-for (var i = 0; i < arr.length; i++) {
-    sum += arr[i]; // sum = sum + arr[i]
+for (var i = 0; i < arr1.length; i++) {
+    sum += arr1[i]; // sum = sum + arr[i]
 }
 console.log(sum);
+
+var arr2 = [1, 4, 3, 9, 8, 5, 7];
+for(var i = 0;i<arr.length;i++){
+    if(arr2[i]%2== 0){
+        console.log(arr2[i])
+    }
+}
