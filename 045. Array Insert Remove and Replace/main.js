126109ed6c801abe94fe32b288408c68804c579c
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-09 12:27:30 +0600

    remove element

--- 045. Array Insert Remove and Replace/main.js
+++ 045. Array Insert Remove and Replace/main.js
@@ -17,3 +17,20 @@ console.log(arr);
 
 arr.splice(2, 0, 1000);
 console.log(arr);
+
+var arr1 = [1, 2, 3, 4, 5, 6, 7, 8];
+
+/* arr[2] = undefined;
+console.log(arr) */
+
+arr1.pop();
+console.log(arr1);
+
+arr1.shift();
+console.log(arr1);
+
+arr1.splice(2, 1);
+console.log(arr1);
+
+arr1.splice(3, 1, 33, 44);
+console.log(arr1);
