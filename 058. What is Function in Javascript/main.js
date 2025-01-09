2d390f19c90e58886b3b7440e85063ce0d301c79
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-09 20:53:22 +0600

    defination of function

---
+++ 058. What is Function in Javascript/main.js
@@ -0,0 +1,6 @@
+/* 
+    In JavaScript, a function is a block of code designed to perform a particular task. Functions can take inputs (called parameters or arguments) and return a result (an output). Functions allow you to reuse code, organize it into modular units, and improve the readability and maintainability of your code.
+*/
+
+var date = new Date();
+date.getFullYear();
