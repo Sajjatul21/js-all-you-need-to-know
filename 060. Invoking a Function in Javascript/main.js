d86db4e924e272483f9773280bc7adbfc722240e
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-09 21:05:02 +0600

    invoking

---
+++ 060. Invoking a Function in Javascript/main.js
@@ -0,0 +1,26 @@
+function functionName() {
+    console.log("I am a function");
+}
+
+function add() {
+    var a = 10;
+    var b = 20;
+    console.log(a + b);
+}
+
+function sub() {
+    var a = 10;
+    var b = 20;
+    console.log(a - b);
+}
+
+functionName();
+
+for (var i = 5; i < 5; i++) {
+    functionName();
+}
+
+add();
+sub();
+
+
