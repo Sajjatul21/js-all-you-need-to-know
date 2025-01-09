85c81b1295da2903c65c2d2583e2ee671390f8dc
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-09 11:58:54 +0600

    sum all array element

--- 044. JS Array Traversing/main.js
+++ 044. JS Array Traversing/main.js
@@ -5,3 +5,10 @@ for (var i = 0; i < arr.length; i++) {
     arr[i] = arr[i] + 2;
     console.log(arr[i]);
 }
+
+var arr1 = [5, 4, 3, 10, 8, 2, 7];
+var sum = 0;
+for (var i = 0; i < arr.length; i++) {
+    sum += arr[i]; // sum = sum + arr[i]
+}
+console.log(sum);
