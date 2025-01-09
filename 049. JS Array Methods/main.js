4b2451474c7443f82f8d340c2b291689bd53e575
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-09 16:47:49 +0600

    fro m method

--- 049. JS Array Methods/main.js
+++ 049. JS Array Methods/main.js
@@ -5,12 +5,26 @@ console.log(arr.join(' '));
 console.log(arr.join(', '));
 console.log(arr.join('| '));
 
-arr.fill(0)
-console.log(arr)
-arr.fill(true)
-console.log(arr)
+arr.fill(0);
+console.log(arr);
+arr.fill(true);
+console.log(arr);
 
 var arr1 = [3, 4, 5, 6];
-var arr2 = [7,8,9,10];
-var arr3 = arr1.concat(arr2)
-console.log(arr3)
+var arr2 = [7, 8, 9, 10];
+var arr3 = arr1.concat(arr2);
+console.log(arr3);
+
+console.log(Array.isArray(arr));
+
+/* var a = [1,2]
+var b = a
+b[0] = 5
+console.log(a)
+console.log(b) */
+
+var a = [1, 2];
+var b = Array.from(a);
+b[0] = 5;
+console.log(a);
+console.log(b);
