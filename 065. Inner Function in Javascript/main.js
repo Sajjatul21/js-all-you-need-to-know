cd5e75fd9a701c73ca50e8318e5bee78d34475b7
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-10 08:49:53 +0600

    another inner function

--- 065. Inner Function in Javascript/main.js
+++ 065. Inner Function in Javascript/main.js
@@ -6,3 +6,19 @@ function something(greet, name) {
 }
 
 something('Good Morning', "Shihab");
+
+function message(greet, name) {
+    function getFirstName() {
+        if (name) {
+            return name.split(' ')[0];
+        }
+        else {
+            return '';
+        }
+    }
+    var message = greet + " " + getFirstName();
+    console.log(message);
+}
+
+message("Good Morning", "Sajjatul Islam");
+message("Good Morning");
