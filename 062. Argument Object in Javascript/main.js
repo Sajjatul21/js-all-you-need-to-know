8d4c1c97a6b7c4022f61211dd4e10b2a0c8311eb
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-10 07:32:06 +0600

    when we use arguments we can pass more and more argument as we need

--- 062. Argument Object in Javascript/main.js
+++ 062. Argument Object in Javascript/main.js
@@ -6,3 +6,13 @@ function test() {
 }
 // test();
 test(1, 3, 4);
+
+function addAll() {
+    var sum = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    console.log(sum)
+}
+addAll(1,2,3,4)
+addAll(1,2,3,4,6,7,8,9,10)
