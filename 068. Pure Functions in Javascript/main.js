7da991e3a085d12113e67e59169ec540bdd76a63
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-10 16:06:52 +0600

    its has side effect its change n value and change point object. thay whay its not pure function

--- 068. Pure Functions in Javascript/main.js
+++ 068. Pure Functions in Javascript/main.js
@@ -8,3 +8,27 @@ function sqr(n) {
 console.log(sqr(2));
 console.log(sqr(2));
 console.log(sqr(2));
+
+
+var n = 10;
+
+function change() {
+    n = 100;
+}
+change();
+console.log(n);
+
+
+var point = {
+    x: 10, 
+    y: 20
+}
+
+function printPoint (point){
+    point.x = 100
+    point.y = 200
+    console.log(point)
+}
+
+printPoint(point)
+console.log(point)
