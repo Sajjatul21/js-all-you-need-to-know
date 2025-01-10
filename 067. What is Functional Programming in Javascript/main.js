f304ecd8d7391029bfcc1068e7709f9d0a0aa2f7
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-10 15:47:23 +0600

    porgramming pradime

---
+++ 067. What is Functional Programming in Javascript/main.js
@@ -0,0 +1,13 @@
+//  what is function programming
+
+/* 
+    Functional programming is a programming programming paradigm - stye of balding the structure and elements of computer programs -that
+    treat commotion as evaluation of mathematical functional avoids changing0state and mutable data- wikipedia
+*/
+
+/* three main terms 
+    1. pure function
+    2. First Class function
+    3. Higher Order Function
+
+*/
