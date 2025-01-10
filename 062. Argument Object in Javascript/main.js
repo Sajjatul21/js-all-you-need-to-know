574d48eb224486f9684879c1dd8f31d2cb34a003
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-10 07:28:01 +0600

    when we use agrument we not need use parameter. but use parameter use best practice

--- 062. Argument Object in Javascript/main.js
+++ 062. Argument Object in Javascript/main.js
@@ -1,5 +1,8 @@
-function test(a, b, c) {
-    console.log(arguments);
+function test() {
+    // console.log(arguments);
+    for (var i = 0; i < arguments.length; i++) {
+        console.log(arguments[i]);
+    }
 }
 // test();
 test(1, 3, 4);
