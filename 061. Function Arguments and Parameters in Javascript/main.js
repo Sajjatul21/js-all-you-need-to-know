0c84de69bfc0a11dcfc51bc806d067cd649d9a61
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-10 06:58:19 +0600

    argument and parmeter

---
+++ 061. Function Arguments and Parameters in Javascript/main.js
@@ -0,0 +1,15 @@
+/* function add (var a, var b ){ // parameter act like variable
+
+} */
+
+function add(a, b) {
+    var result = a + b;
+    console.log(result);
+}
+add(10, 20)
+
+function sub(a, b ){
+    var result = a -b;
+    console.log(result)
+}
+sub (20,5)
