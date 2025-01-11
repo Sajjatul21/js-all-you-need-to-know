97c4b28a7b623a84d1407e15c50314a9b77870d8
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-11 09:27:47 +0600

    closure

---
+++ 071. Closure First Look in Javascript/main.js
@@ -0,0 +1,26 @@
+// First look at Closure
+
+//If data comes from an external scope inside a scope, we call it a closure.
+
+var b = 10;
+
+function a() {
+    console.log(b);
+}
+
+a();
+
+
+// another example  /যদি কোন স্কোপ এর ভিতরে বাহিরের কোন স্কোপ থেকে কোন ডাটা আসে তাহলে তাকে আমরা ক্লোজার বলে।
+function outer() {
+    let outerVariable = 'I am from outer function';
+
+    function inner() {
+        console.log(outerVariable);  // inner() has access to outerVariable
+    }
+
+    return inner;
+}
+
+const closureExample = outer(); // outer() returns inner() function
+closureExample(); // Logs: "I am from outer function"
