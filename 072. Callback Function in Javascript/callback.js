87a70d8605cd13545a6001e57527821432407129
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-12 08:32:55 +0600

    create a normal function

---
+++ 072. Callback Function in Javascript/callback.js
@@ -0,0 +1,24 @@
+/*           "I will call back later!"
+
+A callback is a function passed as an argument to another function
+
+This technique allows a function to call another function
+
+A callback function can run after another function has finished
+*/
+// https://www.w3schools.com/js/js_callback.asp
+
+// https://www.youtube.com/watch?v=0eSG3_mvFZY
+
+
+function display(some) {
+    console.log(some);
+}
+
+function calculator(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+
+let result = calculator(5, 5);
+display(result);
