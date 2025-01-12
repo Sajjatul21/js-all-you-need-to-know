bf49ceac4affd16e3c6fa666504584269ceb07f1
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-12 10:21:22 +0600

    map emplementation

--- 074. Map Function in Javascript/main.js
+++ 074. Map Function in Javascript/main.js
@@ -26,14 +26,31 @@ var sqrArr = arr.map(function (value, index, arr) {
 console.log(arr);
 console.log(sqrArr);
 
-function map(arr) {
+function map(arr, callback) {
     var newArray = [];
     for (var i = 0; i < arr.length; i++) {
         // console.log(arr[i]);
-        var temp = arr[i] * arr[i];
+        var temp = callback(arr[i], i, arr);
         newArray.push(temp);
     }
-    return newArray
+    return newArray;
 }
 
-console.log(map(arr));
+var myMap = map(arr, function (value, index, arr) {
+    return value * value;
+});
+console.log(myMap);
+
+
+var qubArr = map(arr, function (value) {
+    return value * value * value;
+});
+
+console.log(qubArr);
+
+
+var arrTen = map(arr, function (value) {
+    return value * 10;
+});
+
+console.log(arrTen);
