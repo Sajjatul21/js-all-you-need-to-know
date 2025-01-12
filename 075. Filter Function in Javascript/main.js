d9ef17733f53a1709ff2084571d3cdad01073174
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-12 10:43:25 +0600

    filter method

---
+++ 075. Filter Function in Javascript/main.js
@@ -0,0 +1,29 @@
+
+// The `filter()` method creates a new array based on various conditions from our existing array.
+// Only those elements from the original array that meet the specified condition are included in the new array.
+// It doesn't modify the original array but returns a new one with the filtered elements.
+
+/* 
+ Why 
+   Use filter() in JavaScript?
+The filter() method in JavaScript is used to create a new array containing elements from the original 
+array that meet a specific condition. It is ideal when you want to extract a subset of data based on certain criteria.
+*/
+
+/* 
+    Syntax of filter()
+
+    const newArray = array.filter(function(element, index, array) {
+    // Return true to include the element, false to exclude it
+});
+
+*/
+
+var arr = [2, 5, 2, 7, 8, 5, 4, 9, 5, 8, 4];
+
+var filterArr = arr.filter(function (value) {
+    // return value % 2 == 0;
+    // return value % 2 == 1;
+    return value > 4;
+});
+console.log(filterArr);
