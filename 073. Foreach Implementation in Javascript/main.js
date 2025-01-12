51466989339634aeaaead7fd1b231bd72d34a5ae
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-12 09:13:35 +0600

    using forEach

---
+++ 073. Foreach Implementation in Javascript/main.js
@@ -0,0 +1,14 @@
+// When to Use forEach in JavaScript?
+// The forEach method in JavaScript is used to iterate over the elements of an array. It is best suited for cases where you need to perform an operation on each element of the array without returning a new array.
+
+/* 
+    array.forEach(function(element, index, array) {
+        // Code to execute on each element
+});
+
+*/
+
+var arr = [1, 2, 3, 4, 5];
+arr.forEach(function (value, index, arr) {
+    console.log(value, index, arr);
+});
