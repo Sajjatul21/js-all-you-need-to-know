be9aeec684c6668166aeff260844ba14bf7ed08e
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-13 21:48:54 +0600

    descending order

--- 078. Sort, Some and Every Function in Javascrip/main.js
+++ 078. Sort, Some and Every Function in Javascrip/main.js
@@ -22,10 +22,10 @@ var persons = [
 var arr = [5, 4, 3, 8, 1, 7, 0, 6, 5, 7, 2, -1, -4, -8];
 arr.sort(function (a, b) {
     if (a > b) {
-        return 1;
+        return -1;
     }
     else if (a < b) {
-        return -1;
+        return 1;
     }
     else {
         return 0;
