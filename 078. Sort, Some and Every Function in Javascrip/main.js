06a09a87802c163974aa4d174acca00f71527344
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-13 21:47:53 +0600

    when have negative number in array shot method no working properly then we need the condition

--- 078. Sort, Some and Every Function in Javascrip/main.js
+++ 078. Sort, Some and Every Function in Javascrip/main.js
@@ -19,6 +19,16 @@ var persons = [
     }
 ];
 
-var arr = [5, 4, 3, 8, 1, 7, 0, 6, 5, 7, 2];
-arr.sort();
+var arr = [5, 4, 3, 8, 1, 7, 0, 6, 5, 7, 2, -1, -4, -8];
+arr.sort(function (a, b) {
+    if (a > b) {
+        return 1;
+    }
+    else if (a < b) {
+        return -1;
+    }
+    else {
+        return 0;
+    }
+});
 console.log(arr);
