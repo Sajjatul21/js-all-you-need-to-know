2bac6654c578d424274bed7435ea18c9b8e3ddaa
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-13 21:44:26 +0600

    sort

---
+++ 078. Sort, Some and Every Function in Javascrip/main.js
@@ -0,0 +1,24 @@
+// sort change main array 
+
+var persons = [
+    {
+        name: "A",
+        age: 24
+    },
+    {
+        name: "B",
+        age: 19
+    },
+    {
+        name: "C",
+        age: 25
+    },
+    {
+        name: "D",
+        age: 21
+    }
+];
+
+var arr = [5, 4, 3, 8, 1, 7, 0, 6, 5, 7, 2];
+arr.sort();
+console.log(arr);
