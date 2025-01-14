ec25aa21d1beab05c0b52e66f804d09cb370ce54
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-14 08:30:40 +0600

    power using returnfunction from another function

--- 079. Return Function from Another Function in Javascript/main.js
+++ 079. Return Function from Another Function in Javascript/main.js
@@ -13,3 +13,24 @@ console.log(msg);
 
 var hello = greet("Hello");
 console.log(hello('Shihab'));
+
+
+
+function base(b) {
+    return function (n) {
+        var result = 1;
+        for (var i = 0; i < b; i++) {
+            result *= n;
+        }
+        return result;
+    };
+
+}
+var base10 = base(10);
+console.log(base10(2));  //2^10=1,024
+
+
+var base5 = base(5);
+console.log(base5(2));
+console.log(base5(3));
+console.log(base5(5));
