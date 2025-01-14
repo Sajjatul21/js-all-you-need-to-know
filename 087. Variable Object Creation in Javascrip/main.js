73e77e873c106265ce23f6f7436b09078fe835a3
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-14 12:43:21 +0600

    variable creation and execution

---
+++ 087. Variable Object Creation in Javascrip/main.js
@@ -0,0 +1,25 @@
+var a = 10;
+
+var b;
+
+console.log("a is", a);
+console.log("b is", b);
+
+b = 20;
+
+console.log("b is ", b);
+console.log("c is", c);
+
+var c = 30;
+
+console.log("c is", c);
+
+// creation phase
+// a = undefine
+// b = undefine
+// c = undefine
+
+// execution phase
+// a = 10;
+// b = 20;
+// c = 30;
