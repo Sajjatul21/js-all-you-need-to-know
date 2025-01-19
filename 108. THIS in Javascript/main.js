690de278e68454c9940529841f35cf3b223e45e9
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-20 05:59:58 +0600

    this

--- 108. THIS in Javascript/main.js
+++ 108. THIS in Javascript/main.js
@@ -1,5 +1,5 @@
 // this
-// ১.this যেই objectএর মধ্যে ব্যবহার করা হবে সেই objet কে refer করবে।
+// ১.this যেই object এর মধ্যে ব্যবহার করা হবে সেই objet কে refer করবে।
 // ২. object ব্যতিত this window object refer করবে।
 // ৩. this এর মান execution context এর execution phase এর পেয়ে থাকে
 
@@ -7,5 +7,27 @@
 function myFunc() {
     console.log(this);
 }
-myFunc()
-new myFunc()
+myFunc();
+new myFunc();
+
+// 
+var rect = {
+    width: 100,
+    height: 50,
+
+    draw: function () {
+        console.log('I am a rectangle');
+        this.printProperties(); // when you use a method or property on object using this
+        console.log(this); //object ব্যতিত this window object refer করবে।
+
+    },
+    printProperties: function () {
+        console.log("My width is " + this.width);
+        console.log("My height is " + this.height);
+
+    }
+};
+
+
+rect.draw();
+
