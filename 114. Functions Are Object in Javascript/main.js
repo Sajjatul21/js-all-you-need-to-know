33dbabfe679feb664be24c7de0a906dd3051e6e0
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-22 07:51:45 +0600

    function

---
+++ 114. Functions Are Object in Javascript/main.js
@@ -0,0 +1,44 @@
+/*
+var Rectangle = function (width, height) {
+    this.width = width;
+    this.height = height;
+
+    this.draw = function () {
+        console.log('I am a rectangle');
+        this.printProperties();
+        console.log(this);
+
+    };
+    this.printProperties = function () {
+        console.log("My width is " + this.width);
+        console.log("My height is " + this.height);
+
+    };
+}; */
+
+// In javaScript function is a object. it has constructor property is also have object like property
+function test() {
+    console.log("Something");
+}
+
+
+var Rect = new Function('width', 'height', `
+    this.width = width;
+    this.height = height;
+
+    this.draw = function () {
+        console.log('I am a rectangle');
+        this.printProperties();
+        console.log(this);
+
+    };
+    this.printProperties = function () {
+        console.log("My width is " + this.width);
+        console.log("My height is " + this.height);
+
+    };
+    `);
+
+var rect = new Rect(4, 5);
+console.log(rect);
+rect.draw();
