440f23138d4f4b5282a2f6886c2f76c26c75b19a
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-23 23:49:59 +0600

    normal functio

---
+++ 115. Bind, Call, Apply in Javascript/main.js
@@ -0,0 +1,12 @@
+/*
+ যখ ন আমরা normal function call করি তখন সে window object কে refer করে । আমরা
+ বলে দিতে পারি call, apply, bind এর মাধ্যমে তুমি window object refer না করে আমাদের
+ দেওয়া object কে refer করো।
+*/
+// একাটা function যখন different object share করে তখন আমরা  call, apply, bind use করব।
+
+function myFunc() {
+    console.log(this.a + this.b); //a এবং b-এর মান window object নাই তাই তাদের মান undefine আসছে । undefine undefine যোগ করলে আসবে।
+}
+myFunc();
+
