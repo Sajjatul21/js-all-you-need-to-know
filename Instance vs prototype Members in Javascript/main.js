023a70acfdda40b17173d93bdf3769995979b0a4
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-28 15:28:44 +0600

    set draw on prototype

--- Instance vs prototype Members in Javascript/main.js
+++ Instance vs prototype Members in Javascript/main.js
@@ -1,11 +1,13 @@
 function Square(width) {
     this.width = width;
 
-    this.draw = function () {
-        console.log("Draw");
-    };
-}
+};
 
+Square.prototype = {  // prototype is an object
+    draw: function () {
+        console.log("Draw");
+    }
+};
 var sqr1 = new Square(5);
 var sqr2 = new Square(10);
 console.log(sqr1); // instance/ object
