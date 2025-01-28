9ac620fa67d03e31ae53af86166c186d0882e3c6
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-28 21:43:30 +0600

    previous code

---
+++ 133. Method Overriding in  Javascript/main.js
@@ -0,0 +1,34 @@
+function extend(child, parent) {
+    child.prototype = Object.create(parent.prototype);
+    child.prototype.constructor = child;
+}
+function Shape(color) {
+    this.color = color;
+
+}
+Shape.prototype.common = function () {
+    console.log("I am common function");
+};
+
+function Square(width, color) {
+    Shape.call(this, color);
+    this.width = width;
+}
+
+extend(Square,Shape)
+Square.prototype.draw = function () {
+    console.log("Drawing");
+};
+
+
+
+var sqr = new Square(45, "green");
+console.log(sqr);
+
+function Circle(radius, color) {
+    Shape.call(this, color);
+    this.radius = radius;
+}
+extend(Circle,Shape)
+var c = new Circle(5, 'red');
+console.log(c);
