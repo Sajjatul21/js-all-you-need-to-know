ed2039b04fbafbef4424ce0f5123dce4eac9a25f
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-28 20:19:37 +0600

    file name update

---
+++ 130. Reset Constructor After Inheritance inJavascript/main.js
@@ -0,0 +1,31 @@
+function Shape() {
+
+}
+Shape.prototype = {
+    common: function () {
+        console.log("I am common function");
+    }
+};
+
+function Square(width) {
+    this.width = width;
+}
+// Object.create(Shape.prototype);
+Square.prototype = Object.create(Shape.prototype);
+Square.prototype.draw = function () {
+    console.log("Drawing");
+};
+
+var sqr = new Square(45);
+var shape = new Shape();
+console.log(sqr);
+console.log(shape);
+
+
+function Circle() {
+
+}
+Circle.prototype = Object.create(Shape.prototype);
+
+var circle = new Circle();
+console.log(circle);
