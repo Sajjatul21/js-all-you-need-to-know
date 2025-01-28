f7e9b08b2b5bed7d73be1ba6a13440ace881f084
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-28 21:25:57 +0600

    previous code

---
+++ 132. Creating Extends Function in Javascript/main.js
@@ -0,0 +1,24 @@
+function Shape(color) {
+    this.color = color;
+
+}
+Shape.prototype.common = function () {
+    console.log("I am common function");
+};
+
+function Square(width, color) {
+    Shape.call(this, color);
+    this.width = width;
+}
+
+Square.prototype = Object.create(Shape.prototype);
+Square.prototype.constructor = Square;
+Square.prototype.draw = function () {
+    console.log("Drawing");
+};
+
+
+
+var sqr = new Square(45, "green");
+console.log(sqr);
+
