685a1ee8e3f608bd5cf2a18c8238617753d47044
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-29 15:33:33 +0600

    arrow function

---
+++ 141. Arrow Function Syntax in Javascript/main.js
@@ -0,0 +1,22 @@
+// function declaration
+function add(a, b) {
+        return a + b;
+}
+
+//function expression
+let sum = function () {
+        return a + b;
+};
+
+// arrow function
+
+/* let sum2 = (a, b) => {
+        return a + b;
+};  */
+
+let sum2 = (a, b) => a + b;
+console.log(sum2(2, 3));
+
+// let sqr => (x) = x * x;
+let sqr = x => x * x;
+console.log(sqr(2));
