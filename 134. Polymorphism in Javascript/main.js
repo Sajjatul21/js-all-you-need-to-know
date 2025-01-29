54ee603b081f328b3f8e0e34bd31be351c04da30
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-29 06:49:10 +0600

    polymorphism

--- 134. Polymorphism in Javascript/main.js
+++ 134. Polymorphism in Javascript/main.js
@@ -20,22 +20,33 @@ Square.prototype.draw = function () {
     console.log("Drawing");
 };
 
+Square.prototype.common = function () {
+    console.log("I am Calling from Squre and I have Overriding");
+};
 
-
-var sqr = new Square(45, "green");
-// console.log(sqr);
 
 function Circle(radius, color) {
     Shape.call(this, color);
     this.radius = radius;
 }
 extend(Circle, Shape);
-var c = new Circle(5, 'red');
-// console.log(c.common());
+
+
 Circle.prototype.common = function () {
-    Shape.prototype.common.call(this)
+    Shape.prototype.common.call(this);
     console.log("I am Calling from Circle and I have Overriding");
 };
-console.log(c.common())
 
+
+var s = new Shape("black");
+var sqr = new Square(45, "green");
+var c = new Circle(5, 'red');
+
+console.log(s.common());
 console.log(sqr.common());
+console.log(c.common());
+
+var shapes = [s, sqr, c];
+for(var i of shapes){
+    i.common()
+}
