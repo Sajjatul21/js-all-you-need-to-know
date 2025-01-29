dddcd9f95a5cb89d24c6f0f39230ef47bb1ad880
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-29 20:09:35 +0600

    destructuring

--- 147. Destructuring in Javascript/main.js
+++ 147. Destructuring in Javascript/main.js
@@ -1,9 +1,14 @@
 let person = {
     name: 'Shihab',
-    email: "shihab@example.com"
+    email: "shihab@example.com",
+    address: {
+        city: "Chandpur",
+        country: "Bangladesh"
+    }
 };
-let name = person.name;
-let email = person.email;
+let { name, email, address, address: { city, country } } = person;
 console.log(name);
 console.log(email);
-
+console.log(address);
+console.log(city);
+console.log(country);
