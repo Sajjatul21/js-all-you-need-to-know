13e85d975f1d510844bc0c75cc7483fd3030e605
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-29 15:18:10 +0600

    fixed problem

--- 139. Template String in Javascript/main.js
+++ 139. Template String in Javascript/main.js
@@ -8,7 +8,7 @@ var age = 26;
 var name = "Sajjatul Islam";
 
 console.log('My name is ' + name + " and I am " + age + " years old.");
-console.log(`My name is ${name} and I am ${age} old. I am ${age < 18 ? 'not ' : ''}adult`);
+console.log(`My name is ${name} and I am ${age} old. I am ${age < 18 ? not  : ''}adult`);
 
 
 console.log(name.padStart(25));
