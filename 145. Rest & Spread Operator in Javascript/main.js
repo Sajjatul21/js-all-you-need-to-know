0613b8160a3ba94a2a3e459274624a1f047ca702
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-29 19:52:40 +0600

    spread operator

--- 145. Rest & Spread Operator in Javascript/main.js
+++ 145. Rest & Spread Operator in Javascript/main.js
@@ -9,13 +9,17 @@ console.log(sum(1, 2, 3, 4, 5));
 
 // rest operator
 
-function sum2(a,b,...rest) {  // when 3 dot use on parameter its rest operator
+function sum2(a, b, ...rest) {  // when 3 dot use on parameter its rest operator
     // console.log(rest)// rest operator give us array
 
-    console.log(a,b)
-    console.log(rest)
+    console.log(a, b);
+    console.log(rest);
     return rest.reduce((a, b) => a + b);
 
 }
 
 console.log(sum2(1, 2, 3, 4, 5));
+
+let arr = [1, 2, 3, 4, 5, 6];
+console.log(...arr);
+
