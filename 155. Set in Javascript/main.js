d74427dc49496bd115b4b8e2d62d86fe3ced41f9
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-30 10:39:05 +0600

    set keys and values give us iterator

--- 155. Set in Javascript/main.js
+++ 155. Set in Javascript/main.js
@@ -7,15 +7,24 @@ set.add(5)
 set.add(6)
 console.log(set) */
 
-let set = new Set ([1,2,3])
-set.add(4)
-set.add(5)
-set.add(1)
-set.add(2)
+let set = new Set([1, 2, 3]);
+set.add(4);
+set.add(5);
+set.add(1);
+set.add(2);
 /* console.log(set)
 console.log(set.size) */
 
 // set.clear()
 // set.delete(5)
 // console.log(set.has(4))
-console.log(set)
+// console.log(set)
+
+console.log(set.keys());
+console.log(set.values());
+
+let keyIterate = set.keys();
+console.log(keyIterate.next());
+
+let valueIterate = set.values();
+console.log(valueIterate.next());
