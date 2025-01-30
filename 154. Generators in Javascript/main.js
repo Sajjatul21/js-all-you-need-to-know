d461c1462acbdaa211c3cfdb8da6121ef9fa393f
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-30 08:37:37 +0600

    generator for loop

--- 154. Generators in Javascript/main.js
+++ 154. Generators in Javascript/main.js
@@ -1,10 +1,17 @@
-function* generator() {
+/* function* generator() {
     yield 1;
     yield 2;
     yield 3;
 
+} */
+var arr = [1, 2, 3];
+function* generator(collection) {
+    for (let i = 0; i < collection.length; i++) {
+        yield collection[i];
+    }
 }
-let iterate = generator();
+
+let iterate = generator(arr);
 console.log(iterate.next());
 console.log(iterate.next());
 console.log(iterate.next());
