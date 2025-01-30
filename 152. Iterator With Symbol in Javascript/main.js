2a2574eee00d6de4d2dcf58075993abbba750b85
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-30 07:54:56 +0600

    check is array and string is iterable

--- 152. Iterator With Symbol in Javascript/main.js
+++ 152. Iterator With Symbol in Javascript/main.js
@@ -21,19 +21,5 @@ console.log(iterate.next());
 console.log(iterate.next()); */
 
 var arr = [1, 2, 3];
-let iterate = arr[Symbol.iterator]();
-console.log(iterate);
-console.log(iterate.next());
-console.log(iterate.next());
-console.log(iterate.next());
-console.log(iterate.next());
-
-
-var str = "TEXT";
-let iterateText = str[Symbol.iterator]();
-console.log(iterateText);
-console.log(iterateText.next());
-console.log(iterateText.next());
-console.log(iterateText.next());
-console.log(iterateText.next());
-console.log(iterateText.next());
+console.log(arr[Symbol.iterator])
+console.log("Text"[Symbol.iterator])
