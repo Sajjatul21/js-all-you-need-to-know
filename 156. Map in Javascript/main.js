71aed8d2d007217deba1b52064e8dc7d0f3e9832
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-30 13:14:11 +0600

    for of and forEach

--- 156. Map in Javascript/main.js
+++ 156. Map in Javascript/main.js
@@ -12,8 +12,17 @@ map.delete('objKey');
 // map.delete('c');
 console.log(map);
 console.log(map.size);
-console.log(map.get("d"))
+console.log(map.get("d"));
 
-console.log(map.keys())
-console.log(map.values())
-console.log(map.entries())
+console.log(map.keys());
+console.log(map.values());
+console.log(map.entries());
+
+
+for (let [key, value] of map) {
+    console.log(key, value);
+}
+
+map.forEach(function (value, key) {
+    console.log(value, key);
+});
