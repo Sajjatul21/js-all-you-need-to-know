6e10bf56bac5ea454dd6571569eda75c321a0832
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-30 10:32:59 +0600

    set not add duplicate data

---
+++ 155. Set in Javascript/main.js
@@ -0,0 +1,16 @@
+/* let set = new Set ([1,2,3])
+console.log(set) */
+
+/* let set = new Set ()
+set.add(4)
+set.add(5)
+set.add(6)
+console.log(set) */
+
+let set = new Set ([1,2,3])
+set.add(4)
+set.add(5)
+set.add(1)
+set.add(2)
+console.log(set)
+console.log(set.size)
