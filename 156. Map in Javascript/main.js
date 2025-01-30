d172d24464b12401f868a9bf942453ba12c2809d
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-30 13:10:59 +0600

    map methods

--- 156. Map in Javascript/main.js
+++ 156. Map in Javascript/main.js
@@ -5,5 +5,15 @@ let map = new Map([
     ['d', 4],
 ]);
 map.set('e', 5);
-map.set({ name: 'Shihab' }, 6);
+// map.set({ name: 'Shihab' }, 6);
+let objKey = { name: "Shihab" };
+map.set("objKey", 6);
+map.delete('objKey');
+// map.delete('c');
 console.log(map);
+console.log(map.size);
+console.log(map.get("d"))
+
+console.log(map.keys())
+console.log(map.values())
+console.log(map.entries())
