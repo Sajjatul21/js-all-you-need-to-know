56199b1398ed2ca7e5a326b2f08399b55bde28ec
Sajjatul21 <sajjatulislam565@gmail.com>
2025-01-31 23:33:00 +0600

    weak set

--- 157. Weak Set in Javascript/main.js
+++ 157. Weak Set in Javascript/main.js
@@ -1,7 +1,15 @@
 let a = { a: 10 }, b = { b: 20 };
-let set = new Set([a, b]);
+/* let set = new Set([a, b]);
 // console.log(set);
 a = null;
 console.log(set);
-let  arr =[...set]
-console.log(arr)
+let arr = [...set];
+console.log(arr[0]); */
+
+// let wekSet = new WeakSet([1,2]) // don't allow to primitive data weakset only allow object
+
+let weakSet = new WeakSet([a, b]);
+console.log(weakSet);
+a = null;
+console.log(weakSet.has(a));
+console.log(weakSet.has(b));
