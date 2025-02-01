80ea29df2051f3d88e7f7c05c44614fc88f4628b
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-01 23:24:41 +0600

    has and get

--- 158. Weak Map in Javascript/main.js
+++ 158. Weak Map in Javascript/main.js
@@ -1,3 +1,8 @@
 let a = { a: 10 }, b = { b: 20 };
 let weakMap = new WeakMap([[a, 45], [b, 75]]);
+a = null;
+console.log(weakMap.get(a));
+console.log(weakMap.has(a));
+console.log(weakMap.has(b));
 console.log(weakMap);
+
