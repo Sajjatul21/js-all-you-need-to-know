a10de7a39b52bd9a864c08b08ce952b1c4ba2408
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-05 16:45:53 +0600

    crate a user1 object using User class

--- 161. Static Method in Javascript/main.js
+++ 161. Static Method in Javascript/main.js
@@ -13,3 +13,12 @@ console.log(MathUtility.square(2));
 
 const obj = new MathUtility();
 console.log(obj.cube(3));
+
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const user1 = new User("Sajjatul", 22);
+console.log(user1);
