ec01bfbd9bb0a109ed1a49e4cc4cddd1e900a080
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-05 20:44:16 +0600

    in class automitic use strict mood

--- 162. This in Javascript Class/main.js
+++ 162. This in Javascript Class/main.js
@@ -1,10 +1,24 @@
-'use strict'
-function Shape() {
-    this.draw = function () {
+
+class Person {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+    print() {
+        console.log(this.name, this.email);
+    }
+    test() {
         console.log(this);
-    };
+    }
+    static create() {
+        let person = JSON.parse(str);
+        return new Person(person.name, person.email);
+    }
 }
 
-let s1 = new Shape();
-var anotherDraw = s1.draw;
-anotherDraw();
+let str = '{"name" : "Shihab", "email": "shihab@example.com"}';
+let p1 = Person.create(str);
+// p1.print();
+p1.test();
+let anotherTest = p1.test;
+anotherTest();
