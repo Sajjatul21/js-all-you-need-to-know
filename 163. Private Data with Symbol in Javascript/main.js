d4bf7640567ea01cb2cfa6a06b105396f6b4bfc1
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-05 21:46:27 +0600

    make privet property using symbol

--- 163. Private Data with Symbol in Javascript/main.js
+++ 163. Private Data with Symbol in Javascript/main.js
@@ -1,10 +1,24 @@
+const _radius = Symbol();
+const _name = Symbol();
 class Circle {
-    constructor(radius) {
-        this.radius = radius;
+    constructor(radius, name) {
+        this[_radius] = radius; // funny way to privet value
+        this[_name] = name;
+
     }
     draw() {
         console.log('Drawing...');
     }
 }
-let c1 = new Circle(2);
-c1.draw();
+let c1 = new Circle(2, "Cred");
+console.log(c1);
+
+console.log(Object.getOwnPropertyNames(c1));
+console.log(Object.getOwnPropertySymbols(c1));
+
+let key = Object.getOwnPropertySymbols(c1)[0];
+console.log(c1[key]);
+
+let key2 = Object.getOwnPropertySymbols(c1)[1];
+console.log(key2);
+console.log(c1[key2]);
