121eff14ba26528d428a983d75a5aae550020d21
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-05 17:04:22 +0600

    another static method use

--- 161. Static Method in Javascript/main.js
+++ 161. Static Method in Javascript/main.js
@@ -29,3 +29,24 @@ console.log(user1); */
 
 const admin = User.CreateAdmin();
 console.log(admin);
+
+class Person {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+    print() {
+        console.log(this.name, this.email);
+    }
+    static create() {
+        let person = JSON.parse(str);
+        return new Person(person.name, person.email);
+    }
+}
+
+let str = '{"name" : "Shihab", "email": "shihab@example.com"}';
+let p1 = Person.create(str);
+console.log(p1);
+
+console.log(p1 instanceof Person);
+p1.print();
