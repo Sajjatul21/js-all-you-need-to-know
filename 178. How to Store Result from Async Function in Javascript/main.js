71122d56051c739490a6805a1e0714690ca0641d
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-06 16:27:36 +0600

    you can not store async function in a variable. if you do something you do it asyng function

--- 178. How to Store Result from Async Function in Javascript/main.js
+++ 178. How to Store Result from Async Function in Javascript/main.js
@@ -1,10 +1,8 @@
 function sayName(name) {
-    let result;
+   
     setTimeout(() => {
-        result = name;
-        console.log("I have done...");
+        console.log(name)
     }, 3000);
-    return result;
+    
 }
-let output = sayName("Sajjatul Islam");
-console.log(output);
+sayName("Sajjatul Islam");
