4aa43967bb860076261a28273540b831d33f416f
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-06 12:16:36 +0600

    when try have error try stor working and catch start working

--- 172. Throwing an Error in Javascript/main.js
+++ 172. Throwing an Error in Javascript/main.js
@@ -1,9 +1,10 @@
 try {
     console.log("I am line 1");
     console.log("I am line 2");
+    throw new Error("I am error");
     console.log("I am line 3");
     console.log("I am line 4");
 }
-catch(e){
-    console.log(e.message)
+catch (e) {
+    console.log(e.message);
 }
