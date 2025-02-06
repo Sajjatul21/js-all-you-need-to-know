36bb1078447cc9ab228cfe0b14f522737b401d45
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-06 15:31:29 +0600

    synchronous and asynchronous

---
+++ 176. What is Asynchronous Programming in Javascript/main.js
@@ -0,0 +1,13 @@
+/* var a = 10;
+var b = 20;
+
+var result = a + b;
+console.log(result);
+
+var response = storeResult(result);
+if (response.success) {
+    console.log("Success fully Stored");
+}
+else {
+    console.log("Data store failed");
+} */
