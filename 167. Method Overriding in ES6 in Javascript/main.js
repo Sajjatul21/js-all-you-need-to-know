505c20360fa1c61eefc50eb26df9b5de5926a985
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-06 08:22:48 +0600

    previous code

---
+++ 167. Method Overriding in ES6 in Javascript/main.js
@@ -0,0 +1,23 @@
+class Shape {
+    constructor(color) {
+        this.color = color;
+    }
+    draw() {
+        console.log("Drawing...");
+    }
+}
+
+class Rectangle extends Shape {
+    constructor(color, width, height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+    calculate() {
+        return this.width * this.height;
+    }
+}
+let r = new Rectangle("Green", 4, 3);
+console.log(r);
+r.draw();
+console.log(r.color);
