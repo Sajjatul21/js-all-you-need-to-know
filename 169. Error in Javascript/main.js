8af5e47869abad024cc0bdae81f89e6f59bc0519
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-06 11:01:33 +0600

    error

---
+++ 169. Error in Javascript/main.js
@@ -0,0 +1,15 @@
+let n = 10;
+/* if (n > 5) {
+    throw new Error("N is Greater Than 5");
+} */
+
+    // range error
+/* if (n > 5) {
+    throw new RangeError("The Number is too long");
+}  */
+
+    //reference error
+/*     console.log(a) */
+
+// syntax error
+console.log(a
