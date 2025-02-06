e3845a71c5166539a947256807547dd89e8e64da
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-06 10:43:42 +0600

    when we emport all export use * as

--- 168. ES6 Modules System in Javascript/main.js
+++ 168. ES6 Modules System in Javascript/main.js
@@ -1,7 +1,14 @@
-import Rectangle from "./Rectangle.js";
-
+/* import Rectangle from "./Rectangle.js";
 
 let r = new Rectangle("Green", 4, 3);
 console.log(r);
 r.draw();
-console.log(r.color);
+console.log(r.color); */
+
+import * as func from './func.js';
+console.log(func);
+
+console.log(func.add(2, 2));
+console.log(func.sub(5, 2));
+console.log(func.times(7, 2));
+console.log(func.div(18, 2));
