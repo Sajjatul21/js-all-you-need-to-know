8aaa0df1e0768873416b6aec13dff7c17f44ed14
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-06 12:15:20 +0600

    when try haven`t any error catch not working

---
+++ 172. Throwing an Error in Javascript/main.js
@@ -0,0 +1,9 @@
+try {
+    console.log("I am line 1");
+    console.log("I am line 2");
+    console.log("I am line 3");
+    console.log("I am line 4");
+}
+catch(e){
+    console.log(e.message)
+}
