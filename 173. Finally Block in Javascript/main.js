dccd8804e9101e332bd4bfc1e4e154b9377a09c3
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-06 12:22:27 +0600

    Whether there is an error or not I have to do something then we will use finaloy block

--- 173. Finally Block in Javascript/main.js
+++ 173. Finally Block in Javascript/main.js
@@ -1,11 +1,12 @@
 try {
     console.log("I am line 1");
+    throw new Error("I am error");
     console.log("I am line 2");
     console.log("I am line 3");
 }
 catch (e) {
     console.log(e.message);
 }
-finally{
-    console.log("I am finally block")
+finally {
+    console.log("I am finally block");
 }
