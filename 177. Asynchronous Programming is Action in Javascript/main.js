f531b19f607a4cf02c5f7f9a24283f085a14ce78
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-06 15:51:23 +0600

    asynchronous

---
+++ 177. Asynchronous Programming is Action in Javascript/main.js
@@ -0,0 +1,14 @@
+
+//Synchronous: একটার পর একটা কাজ সিরিয়ালি (ক্রমানুসারে) সম্পন্ন হবে।
+/* 
+    console.log("Step 1: Start")
+    console.log("Step 2: Processing...")
+    console.log("Step 3: End") 
+*/
+
+// Asynchronous: কোনো কাজ চলতে চলতে অন্য কাজও শুরু হতে পারে।
+console.log("Step 1: Start");
+setTimeout(() => {
+    console.log("Step 2: Processing...");
+}, 2000);
+console.log("Step 3: End");
