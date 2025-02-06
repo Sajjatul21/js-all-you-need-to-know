f2c3bf1326d65dc96488f649b2758fd9495f32d4
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-06 12:56:30 +0600

    custor error

--- 175. Custom Error in Javascript/main.js
+++ 175. Custom Error in Javascript/main.js
@@ -1,13 +1,25 @@
+class CustomError extends Error {
+    constructor(msg) {
+        super(msg);
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, CustomError);
+        }
+    }
+}
+
 try {
     console.log("I am line 1");
-    throw new Error("I am error");
+    // throw new Error("I am error");
+    throw new CustomError("I am your Error")
     console.log("I am line 2");
     console.log("I am line 3");
 }
-catch {
-    // console.log(e.message);
-    console.log("This is custom Error Message");
+catch (e){
+    console.log(e);
+    console.dir(e);
+    console.log(e.message)
+    // console.log("This is custom Error Message");
 }
-finally {
+/* finally {
     console.log("I am finally block");
-}
+} */
