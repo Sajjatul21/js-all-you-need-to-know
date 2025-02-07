7efb5c8c7ce4177f9fca7424998dbd6f8fb33a1f
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-07 16:31:03 +0600

    arrow function and bind method

--- 185. Callback Not Always Asynchronous in Javascript/main.js
+++ 185. Callback Not Always Asynchronous in Javascript/main.js
@@ -3,11 +3,14 @@ let arr = [1, 2, 3, 4, 5];
 let sqrArr = arr.map(v => v * v);
 console.log(sqrArr);
 
-function asyncMap(arr, callback){
-    return arr.map(v=>{
-        setTimeout(callback(v),0)
-    })
+function asyncMap(arr, callback) {
+    return arr.map(v => {
+        // setTimeout(callback.bind(null, v), 0);
+        setTimeout(()=>callback( v), 0);
+    });
 }
 
-let qbArr = asyncMap(arr, v=> v*v*v)
+let qbArr = asyncMap(arr, v => {
+    console.log(v);
+});
 console.log(qbArr)
