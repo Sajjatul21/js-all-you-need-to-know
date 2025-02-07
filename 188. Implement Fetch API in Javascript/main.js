08136d170f8d576bdd8e7a2861af6d752447b84d
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-07 23:48:29 +0600

    fetch

--- 188. Implement Fetch API in Javascript/main.js
+++ 188. Implement Fetch API in Javascript/main.js
@@ -1,10 +1,9 @@
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
-fetch(`${BASE_URL}/user/ 1`)
-    .then((res) => {
-        console.log(res);
-        console.log(res.json());
-    })
+fetch(`${BASE_URL}/users/1`)
+    .then((res) => res.json())
+    .then(data => console.log(data))
     .catch((e) => {
         console.log(e);
     });
+
