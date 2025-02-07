4df72e826333aa9cb0c9635849e66386f6b9017e
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-07 15:29:18 +0600

    ajax implementation

---
+++ 183. What is Ajax/main.js
@@ -0,0 +1,17 @@
+const xhr = new XMLHttpRequest();
+xhr.open('get', 'https://jsonplaceholder.typicode.com/users');
+
+xhr.onreadystatechange = function (e) {
+    if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+            // console.log(xhr.response);
+            let response = JSON.parse(xhr.response);
+            console.log(response);
+        }
+        else {
+            console.log(xhr.status);
+        }
+    }
+};
+
+xhr.send();
