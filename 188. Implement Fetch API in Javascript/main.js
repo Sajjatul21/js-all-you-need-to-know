6c1b7c151e2d56080be1dad907440e4b568f747f
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-08 00:25:20 +0600

    get value using promise

--- 188. Implement Fetch API in Javascript/main.js
+++ 188. Implement Fetch API in Javascript/main.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
-fetch(`${BASE_URL}/users/1`)
+/* fetch(`${BASE_URL}/users/1`)
     .then((res) => res.json())
     .then(data => {
         console.log(data);
@@ -17,24 +17,36 @@ fetch(`${BASE_URL}/users/1`)
     .catch((e) => {
         console.log(e);
     });
-
+ */
 
 
 function getRequest(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('get', url);
+    return new Promise((resolve, reject) => {
 
-    xhr.onreadystatechange = function (e) {
-        if (xhr.readyState == 4) {
-            if (xhr.status == 200) {
-                let response = JSON.parse(xhr.response);
-                callback(null, response);
+        const xhr = new XMLHttpRequest();
+        xhr.open('get', url);
+
+        xhr.onreadystatechange = function (e) {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    let response = JSON.parse(xhr.response);
+                    // callback(null, response);
+                    resolve(response);
+                }
+                else {
+                    // callback(xhr.status, null);
+                    // reject(new Error(xhr.status));
+                    reject(new Error('Error occurred'));
+                }
             }
-            else {
-                callback(xhr.status, null);
-            }
-        }
-    };
-    xhr.send();
+        };
+        xhr.send();
+    });
 }
 
+/* let res =getRequest(`${BASE_URL}/users/1`)
+console.log(res) */
+
+getRequest(`${BASE_URL}/users/142`)
+    .then(res => console.log(res))
+    .catch(err => console.log(err.message));
