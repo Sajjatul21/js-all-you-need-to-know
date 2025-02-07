beee1866329b4107cc87f0b9e064478495ce008b
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-08 03:08:29 +0600

    when you have more promise . promise connect eatch other . you can use Promise.all, in promise.all all of the promise is  resolve its working otherwise one condition reject promise.all reject

--- 189. Promise API in Javascript/main.js
+++ 189. Promise API in Javascript/main.js
@@ -7,9 +7,20 @@ delay(2).then(()=>console.log('2 Second Delay'));
 delay(5).then(()=>console.log('5 Second Delay')); */
 
 
-let p1 = Promise.resolve("test");  // instead create a promise and resolved the value 
+/* let p1 = Promise.resolve("test");  // instead create a promise and resolved the value 
 // console.log(p1)
 p1.then(v => console.log(v));
 
 let p2 = Promise.reject('reject');  ///instead create a promise . reject the valuer
 p2.catch(e => console.log(e));
+
+ */
+let p1 = Promise.resolve('One');
+let p2 = Promise.resolve('Two');
+let p3 = Promise.resolve('Three');
+
+let promiseArr = [p1, p2, p3];
+Promise.all(promiseArr)
+    .then(arr => {
+        console.log(arr);
+    });
