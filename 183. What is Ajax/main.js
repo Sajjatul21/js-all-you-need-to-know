270aef665486967a8202c15469c7a6b6f214e90a
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-07 16:00:51 +0600

    ajax using function

--- 183. What is Ajax/main.js
+++ 183. What is Ajax/main.js
@@ -1,17 +1,37 @@
-const xhr = new XMLHttpRequest();
-xhr.open('get', 'https://jsonplaceholder.typicode.com/users');
+function getRequest(url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("get", url);
 
-xhr.onreadystatechange = function (e) {
-    if (xhr.readyState == 4) {
-        if (xhr.status == 200) {
-            // console.log(xhr.response);
-            let response = JSON.parse(xhr.response);
-            console.log(response);
+    xhr.onreadystatechange = function (e) {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                let response = JSON.parse(xhr.response);
+                callback(null, response);
+            }
+            else {
+                callback(xhr.status, null);
+            }
         }
-        else {
-            console.log(xhr.status);
-        }
+
+    };
+    xhr.send();
+}
+getRequest('https://jsonplaceholder.typicode.com/users', function (error, response) {
+    if (error) {
+        console.log(error);
     }
-};
+    else {
+        console.log(response);
+    }
+});
 
-xhr.send();
+// another request 
+
+getRequest('https://jsonplaceholder.typicode.com/posts',function(err, res){
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log(res)
+    }
+})
