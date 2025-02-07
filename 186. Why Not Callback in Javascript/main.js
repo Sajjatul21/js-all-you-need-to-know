8e646fdab3cee3ec91bcc1dd6be555e4ac3fa0a4
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-07 21:55:02 +0600

    http request

---
+++ 186. Why Not Callback in Javascript/main.js
@@ -0,0 +1,17 @@
+function getRequest(url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('get', url);
+
+    xhr.onreadystatechange = function (e) {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                let response = JSON.parse(xhr.response);
+                callback(null, response);
+            }
+            else {
+                callback(xhr.status, null);
+            }
+        }
+    };
+    xhr.send();
+}
