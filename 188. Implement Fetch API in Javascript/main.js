e27c01fe5ad8495112e1f338c8d3e2e4053f8a35
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-07 23:44:24 +0600

    fetch have a problem. he did not give value first time , he give another resolve promise. we see tha using json()

--- 188. Implement Fetch API in Javascript/main.js
+++ 188. Implement Fetch API in Javascript/main.js
@@ -1,4 +1,10 @@
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
-let res = fetch(`${BASE_URL}/user/ 1`)
-    console.log(res)
+fetch(`${BASE_URL}/user/ 1`)
+    .then((res) => {
+        console.log(res);
+        console.log(res.json());
+    })
+    .catch((e) => {
+        console.log(e);
+    });
