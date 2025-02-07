8d088384475ae5dd4276adea3bcd1e576fbb5500
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-07 23:00:14 +0600

    when promise resolve then get it in then block, when promise reject then get it in catch block

--- 187. Introduction to Promise in Javascript/main.js
+++ 187. Introduction to Promise in Javascript/main.js
@@ -2,8 +2,16 @@ let p1 = new Promise((resolve, reject) => {
     setTimeout(resolve, 5000, 'one');
 });
 
-let p2 = new Promise ((resolve, reject)=>{
-    setTimeout(resolve, 3000, 'tow')
-})
-console.log(p1)
-console.log(p2)
+let p2 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 3000, 'tow');
+});
+
+p1.then(v => {
+    console.log(v);
+});
+
+p2.then(v => {
+    console.log(v);
+}).catch(e => {
+    console.log(e);
+});
