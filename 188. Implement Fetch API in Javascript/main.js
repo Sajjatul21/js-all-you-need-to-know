fe9b214feb1205cadc1f0a124981e5fd2923a804
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-07 23:55:47 +0600

    when resolve a then block then go to next block

--- 188. Implement Fetch API in Javascript/main.js
+++ 188. Implement Fetch API in Javascript/main.js
@@ -2,7 +2,18 @@ const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
 fetch(`${BASE_URL}/users/1`)
     .then((res) => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data);
+        return Promise.resolve("Something");
+    })
+
+    .then(str => {
+        console.log(str);
+        return Promise.resolve("Another Promises");
+    })
+    .then(another => {
+        console.log(another);
+    })
     .catch((e) => {
         console.log(e);
     });
