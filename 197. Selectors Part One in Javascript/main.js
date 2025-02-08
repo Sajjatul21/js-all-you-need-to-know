58f202485f6b8fbd0738f2c2707f66b1d5f880e0
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-08 21:11:26 +0600

    selector

---
+++ 197. Selectors Part One in Javascript/main.js
@@ -0,0 +1,16 @@
+/* console.log(window.document)
+console.log(document)
+console.log(document.body)
+console.log(document.URL) */
+
+let title = document.getElementById("title");
+console.log(title);
+
+let paragraphs = document.getElementsByClassName("lead");
+console.log(paragraphs);
+
+let lists = document.getElementsByTagName("li");
+console.log(lists);
+
+let listItemOne = document.getElementsByName('list-item-one');
+console.log(listItemOne);
