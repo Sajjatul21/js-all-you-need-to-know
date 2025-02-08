d8dafac7ee5676efe3ed2530a4e40ced6f4ce674
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-08 22:36:30 +0600

    find parent and child element

---
+++ 200. How to Traverse DOM Elements in Javascript/main.js
@@ -0,0 +1,8 @@
+let list = document.getElementById('list');
+// console.log(list)
+
+const parent = list.parentElement;
+console.log(parent);
+
+const children = list.children;
+console.log(children);
