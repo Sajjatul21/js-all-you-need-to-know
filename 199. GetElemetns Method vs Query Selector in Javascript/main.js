14f0bae2ec5721e3dc93aa2ffe0187f338a3ef36
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-08 22:15:29 +0600

    HTMLCollection live NodeList not live

---
+++ 199. GetElemetns Method vs Query Selector in Javascript/main.js
@@ -0,0 +1,11 @@
+let li1 = document.getElementsByTagName("li");
+
+let li2 = document.querySelectorAll("li");
+
+/*
+ console.log(li1);
+console.log(li2); 
+*/
+
+console.log(li1.constructor.name);
+console.log(li2.constructor.name);
