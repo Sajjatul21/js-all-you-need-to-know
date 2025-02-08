a64b547adc10b0bc61e0de4d370694282a39ec65
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-08 22:22:54 +0600

    element node mens html element node, and node mens element node , comment node, text node

--- 199. GetElemetns Method vs Query Selector in Javascript/main.js
+++ 199. GetElemetns Method vs Query Selector in Javascript/main.js
@@ -1,6 +1,6 @@
-let li1 = document.getElementsByTagName("li");
+let li1 = document.getElementsByTagName("li"); // Element Node
 
-let li2 = document.querySelectorAll("li");
+let li2 = document.querySelectorAll("li");    // Node . Node mens any node  [Element, Comment, Text]
 
 /*
  console.log(li1);
@@ -9,3 +9,7 @@ console.log(li2);
 
 console.log(li1.constructor.name);
 console.log(li2.constructor.name);
+
+let ul = document.querySelector('ul');
+console.log(ul.children);
+console.log(ul.childNodes);
