1d7fa4f3fa477eca6efd8d99e59120878e105f13
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-08 22:38:54 +0600

    find siblig.

--- 200. How to Traverse DOM Elements in Javascript/main.js
+++ 200. How to Traverse DOM Elements in Javascript/main.js
@@ -6,3 +6,7 @@ console.log(parent);
 
 const children = list.children;
 console.log(children);
+
+// console.log(list.previousSibling)
+console.log(list.previousElementSibling);
+console.log(list.nextElementSibling);
