86d19539f5698cfdd423b00839c6c2a31437fe96
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-09 10:54:07 +0600

    previous code

---
+++ 204. Update & Remove any DOM Elements in Javascrip/main.js
@@ -0,0 +1,29 @@
+
+function createElement(tagName, className, innerHTML) {
+    let tag = document.createElement(tagName);
+    tag.className = className || '';
+    tag.innerHTML = innerHTML || '';
+
+    return tag;
+}
+function append(parent, children) {
+    children.forEach(child => parent.appendChild(child));
+}
+
+let li = createElement('li', 'list-group-item', 'Four');
+let list = document.getElementById('list');
+list.appendChild(li);
+
+let p1 = createElement('p', 'lead', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et sint blanditiis beatae dicta dolorum odio quas laboriosam molestias fuga omnis.');
+let p2 = createElement('p', 'lead', 'Ipsum dolor sit amet consectetur adipisicing elit. Et sint blanditiis beatae dicta dolorum odio quas laboriosam molestias fuga omnis.');
+
+let div = createElement('div');
+append(div, [p1, p2]);
+
+/* let container = document.getElementById('cont');
+container.appendChild(div); */
+
+// list.insertAdjacentElement('beforebegin', div);
+// list.insertAdjacentElement('afterbegin', div);
+// list.insertAdjacentElement('beforeend', div);
+list.insertAdjacentElement('afterend', div);
