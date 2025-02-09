4bf33c27adb3d854054031c5a09510c8e0c243c9
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-09 17:04:55 +0600

    click event

---
+++ 208. Introduction to Event in DOM Manupulation in Javascript/main.js
@@ -0,0 +1,12 @@
+let btn = document.getElementById("btn");
+// console.log(btn);
+
+/* btn.onclick = function (e) {
+    console.log(e);
+};
+ */
+
+btn.addEventListener("click", function (e) {
+    console.log(e);
+    console.log("I have Clicked");
+});
