9bba3abe7cacb2230ffdc8bc920324d83726f034
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-09 12:15:16 +0600

    style using object

--- 207. Styling DOM in Javascript/main.js
+++ 207. Styling DOM in Javascript/main.js
@@ -2,3 +2,14 @@ let title = document.getElementById('title');
 title.style.color = 'Orange';
 title.style.fontSize = '4rem';
 title.style.background = '#DAF7A6';
+
+let styleObj = {
+    background: "#DAF7A6",
+    fontSize: "30px",
+    color: "#f02d2d"
+
+};
+
+let list = document.getElementById('list');
+// Object.assign(list.style, styleObj);
+[...list.children].forEach(li => Object.assign(li.style,styleObj));
