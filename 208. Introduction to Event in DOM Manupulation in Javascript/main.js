c6849cb64aed423456b3ae49a95f9494231464d4
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-09 17:12:40 +0600

    mousemove

--- 208. Introduction to Event in DOM Manupulation in Javascript/main.js
+++ 208. Introduction to Event in DOM Manupulation in Javascript/main.js
@@ -10,3 +10,19 @@ btn.addEventListener("click", function (e) {
     console.log(e);
     console.log("I have Clicked");
 });
+
+let box = document.getElementById("box")
+box.addEventListener('mousemove', function(e){
+    // console.log(e)
+
+   /*  document.getElementById('x-value').innerHTML = e.clientX
+    document.getElementById('y-value').innerHTML = e.clientY
+ */
+
+    document.getElementById('x-value').innerHTML = e.offsetX
+    document.getElementById('y-value').innerHTML = e.offsetY
+
+    if(e.offsetX == 50 && e.offsetY== 50){
+        console.log('50 50')
+    }
+})
