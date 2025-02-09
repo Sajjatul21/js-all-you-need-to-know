aeaff446f3848a3205a95489b8e766dc68c97efe
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-09 17:15:33 +0600

    add elemen by btn click

--- 208. Introduction to Event in DOM Manupulation in Javascript/main.js
+++ 208. Introduction to Event in DOM Manupulation in Javascript/main.js
@@ -6,23 +6,25 @@ let btn = document.getElementById("btn");
 };
  */
 
+let list = document.getElementById("list");
 btn.addEventListener("click", function (e) {
-    console.log(e);
-    console.log("I have Clicked");
+    let item = list.lastElementChild.cloneNode(true);
+    item.innerHTML = 'Newly Created Item';
+    list.appendChild(item);
 });
 
-let box = document.getElementById("box")
-box.addEventListener('mousemove', function(e){
+let box = document.getElementById("box");
+box.addEventListener('mousemove', function (e) {
     // console.log(e)
 
-   /*  document.getElementById('x-value').innerHTML = e.clientX
-    document.getElementById('y-value').innerHTML = e.clientY
- */
+    /*  document.getElementById('x-value').innerHTML = e.clientX
+     document.getElementById('y-value').innerHTML = e.clientY
+  */
 
-    document.getElementById('x-value').innerHTML = e.offsetX
-    document.getElementById('y-value').innerHTML = e.offsetY
+    document.getElementById('x-value').innerHTML = e.offsetX;
+    document.getElementById('y-value').innerHTML = e.offsetY;
 
-    if(e.offsetX == 50 && e.offsetY== 50){
-        console.log('50 50')
+    if (e.offsetX == 50 && e.offsetY == 50) {
+        console.log('50 50');
     }
-})
+});
