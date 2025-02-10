3e8053b3fb25c05674eadd0779767020e86bcdb4
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-10 21:42:32 +0600

    convert array for using for each

--- 213. Form Handling in DOM in Javascript/main.js
+++ 213. Form Handling in DOM in Javascript/main.js
@@ -2,11 +2,10 @@ let form = document.getElementById('form');
 // console.log(form);
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-    // console.log(event);
-    // console.log(this.length)
-    this.forEach(el => {
-        console.log(el);// we canot use foreach loop because its html collection 
+    /*  console.log(this.elements[0]);
+     console.log(this["email"]); */
+    [...this.elements].forEach(e => {
+        console.log(e.type, e.name, e.value);
     });
-
 
 });
