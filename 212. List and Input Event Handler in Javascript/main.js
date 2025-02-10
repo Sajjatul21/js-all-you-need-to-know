9759b30cb05be5f139bf31e1522772ca08554fb2
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-10 19:49:32 +0600

    previous code

---
+++ 212. List and Input Event Handler in Javascript/main.js
@@ -0,0 +1,38 @@
+let name = document.getElementById('name');
+// console.log(name);
+name.addEventListener('keypress', function (event) {
+    // console.log(event);
+    if (event.key == "Enter") {
+        // console.log(event.target.value);
+        document.getElementById('show-name').innerHTML = `Your Name is ${event.target.value}`;
+        event.target.value = '';
+    }
+});
+
+let skills = document.getElementsByName('skills');
+let parent = document.getElementById('result');
+let checkedSkilled = [];
+
+[...skills].forEach(skill =>
+    skill.addEventListener('change', function (event) {
+        if (event.target.checked) {
+            checkedSkilled.push(event.target.value);
+            outputSkills(parent, checkedSkilled);
+        }
+        else {
+            let ind = checkedSkilled.indexOf(event.target.value);
+            checkedSkilled.splice(ind, 1);
+            outputSkills(parent, checkedSkilled);
+        }
+    })
+);
+
+function outputSkills(parent, skills) {
+    console.log(parent, skills);
+    let result = '';
+    skills.forEach((skill, index) => {
+        result += `(${index + 1}) ${skill} `;
+    });
+    parent.innerHTML = result;
+}
+
