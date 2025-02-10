a56ff5cea626a8450d394591a81063227531e756
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-10 21:54:00 +0600

    get value in an array

--- 213. Form Handling in DOM in Javascript/main.js
+++ 213. Form Handling in DOM in Javascript/main.js
@@ -4,8 +4,16 @@ form.addEventListener('submit', function (event) {
     event.preventDefault();
     /*  console.log(this.elements[0]);
      console.log(this["email"]); */
+    //  console.log(this.elements)
+    let formData = {};
     [...this.elements].forEach(e => {
-        console.log(e.type, e.name, e.value);
+        // console.log(e)
+        // console.log(e.type, e.name, e.value);
+        if (e.type != 'submit') {
+            formData[e.name] = e.value;
+        }
     });
+    console.log(formData);
+    this.reset();
 
 });
