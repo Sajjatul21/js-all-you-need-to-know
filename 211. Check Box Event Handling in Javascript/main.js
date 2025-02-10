fdf2084596d4f2e24894cb38fd0d96dad0b46988
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-10 19:02:57 +0600

    previous code

---
+++ 211. Check Box Event Handling in Javascript/main.js
@@ -0,0 +1,10 @@
+ let name = document.getElementById('name');
+// console.log(name);
+name.addEventListener('keypress', function (event) {
+    // console.log(event);
+    if (event.key == "Enter") {
+        // console.log(event.target.value);
+        document.getElementById('show-name').innerHTML = `Your Name is ${event.target.value}`;
+        event.target.value = '';
+    }
+});
