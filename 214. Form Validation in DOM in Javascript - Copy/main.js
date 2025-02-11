c3fda03ab81d8269b7348bc0f8130472d39f9f9a
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-11 09:25:37 +0600

    create post

--- 214. Form Validation in DOM in Javascript - Copy/main.js
+++ 214. Form Validation in DOM in Javascript - Copy/main.js
@@ -5,7 +5,18 @@ const url = `https://jsonplaceholder.typicode.com/posts`;
 load.addEventListener('click', function () {
     fetch(url)
         .then(response => response.json())
-        .then(data => {
-            console.log(data);
+        .then(posts => {
+            posts.forEach((post, index) => {
+                let listItem = listItemGenerator(post, index + 1);
+                postList.appendChild(listItem);
+            });
         });
 });
+
+function listItemGenerator(item, num) {
+    let li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.innerHTML = `(${num}) ${item.title}.`;
+
+    return li;
+}
