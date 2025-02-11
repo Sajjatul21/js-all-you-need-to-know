f4cb5852ee698f7fd1a79dc163bc9289fafd25b6
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-11 12:34:23 +0600

    set our input data in server

--- 216. Ajax Post Request in Javascript/main.js
+++ 216. Ajax Post Request in Javascript/main.js
@@ -1,22 +1,29 @@
 let postForm = document.getElementById('post-form');
 let posts = document.getElementById('posts');
+let URL = `https://jsonplaceholder.typicode.com/posts`;
 
 postForm.addEventListener('submit', function (e) {
     e.preventDefault();
-    /*   console.log(event)
-  
-      console.log(this.title)
-      console.log(this.body) */
+
     let title = this.title.value || undefined;
     let body = this.body.value || undefined;
-    /*  console.log(title);
-     console.log(body); */
+
     if (title && body) {
         let postObj = {
             userId: 100,
             title,
             body
         };
+        fetch(URL, {
+            method: "POST",
+            headers: {
+                'content-type': 'Application/JSON'
+            },
+            body: JSON.stringify(postObj)
+        })
+            
     }
-
+    else {
+        alert("Please provide Every Details");
+    }
 });
