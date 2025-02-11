487326489262972aa3e7a5c9317d509d32507035
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-11 08:48:20 +0600

    from validation

--- 214. Form Validation in DOM in Javascript/main.js
+++ 214. Form Validation in DOM in Javascript/main.js
@@ -1,14 +1,29 @@
 let form = document.getElementById('form');
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-
+    let isValid = false;
     let formData = {};
     [...this.elements].forEach(e => {
         if (e.type != 'submit') {
-            formData[e.name] = e.value;
+            isValid = validator(e);
+            if (isValid) {
+                formData[e.name] = e.value;
+            }
+            else {
+                alert(`${e.name} is Required`);
+            }
         }
     });
-    console.log(formData);
-    this.reset();
-
+    if (isValid) {
+        console.log(formData);
+        this.reset();
+    }
 });
+
+function validator(el) {
+    console.log(el);
+    if (!el.value) {
+        return false;
+    }
+    return true;
+}
